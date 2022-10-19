38c932d18452adc1adaebfe4e48c5fb57479e47c
Washington Kamadi Kigani <washingtonkigan@gmail.com>
2022-10-19 13:55:35 +0300

    Truth table

--- src/maze_solver.cpp
+++ src/maze_solver.cpp
@@ -1,55 +1,76 @@
 #include <include/maze_solver.h>
 
-maze_solver::maze_solver(wallToFollow wall, double threshold_right, double threshold_front,double threshold_left) {
-    wall_folllow = wall;
-    thresholdFront = threshold_front;
-    thresholdLeft = threshold_left;
-    thresholdRight = threshold_right;
+maze_solver::maze_solver(wallToFollow wall, double threshold_right, double threshold_front, double threshold_left)
+{
+    this->wall_folllow = wall;
+    this->thresholdFront = threshold_front;
+    this->thresholdLeft = threshold_left;
+    this->thresholdRight = threshold_right;
 }
 
-bool maze_solver::WallFound(double threshold, double val){
-    if (val < threshold) {
+bool maze_solver::WallFound(double threshold, double val)
+{
+    if (val < threshold)
+    {
         return true;
-    }else{
+    }
+    else
+    {
         return false;
     }
 }
 
-directions maze_solver::get_turn_directions(double front_dist, double right_dist, double left_dist) {
+directions maze_solver::get_turn_directions(double front_dist, double right_dist, double left_dist)
+{
     bool wallRight, wallLeft, wallFront;
     wallRight = WallFound(thresholdRight, right_dist);
     wallLeft = WallFound(thresholdLeft, left_dist);
     wallFront = WallFound(thresholdFront, front_dist);
 
-    if (wallFront == false && wallLeft == false && wallRight == false){
-        return FOWARD;
-    }else if (wallLeft == false && wallFront == false && wallRight == true) {
-        if (wall_folllow == RIGHT_WALL){
-            return FOWARD;
-        }else {
-            return LEFT;
-        }
-    }else if (wallLeft == false && wallFront == true && wallRight == false) {//POSSIBLE TOSS UP
-        if (wall_folllow == RIGHT_WALL){
-            return RIGHT;
-        }else {
-            return LEFT;
-        }
-    }else if (wallLeft == false && wallFront == true && wallRight == true) {
-        return LEFT;
-    }else if (wallLeft == true && wallFront == false && wallRight == false)
+    // Truth table
+    // Left Front Right Decisiom
+    //  0     0     0      F
+    //  0     0     1      F
+    //  0     1     0      L
+    //  0     1     1      L
+    //  1     0     0      F
+    //  1     0     1      F
+    //  1     1     0      R
+    //  1     1     1      T
+    if (wallLeft == false && wallFront == false && wallRight == false)
     {
-        if (wall_folllow == RIGHT_WALL){
-            return RIGHT;
-        }else {
-            return FOWARD;
-        }
-    }else if (wallLeft == true && wallFront == false && wallRight == true){
         return FOWARD;
-    }else if (wallLeft == true && wallFront == true && wallRight == false){
+    }
+    else if (wallLeft == false && wallFront == false && wallRight == true)
+    {
+        return FOWARD;
+    }
+    else if (wallLeft == false && wallFront == true && wallRight == false)
+    {
+        return LEFT;
+    }
+    else if (wallLeft == false && wallFront == true && wallRight == true)
+    {
+        return LEFT;
+    }
+    else if (wallLeft == true && wallFront == false && wallRight == false)
+    {
+        return FOWARD;
+    }
+    else if (wallLeft == true && wallFront == false && wallRight == true)
+    {
+        return FOWARD;
+    }
+    else if (wallLeft == true && wallFront == true && wallRight == false)
+    {
         return RIGHT;
-    }else{
+    }
+    else if (wallLeft == true && wallFront == true && wallRight == true)
+    {
+        return TURN_AROUND;
+    }                     
+    else
+    {
         return TURN_AROUND;
     }
-    
 }
